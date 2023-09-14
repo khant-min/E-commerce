23da1745d067ae689c55194d0ffb6947e3ea6474
jdev-khantminthu <khantminthu@jdev.com.mm>
2023-09-14 23:07:07 +0630

    remove: unnecessary data

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,16 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
+const router = express.Router({ mergeParams: true });
+
 const PORT = process.env.PORT || 8080;
 
-app.listen(() => console.log(`Server is running at port -> ${PORT}`));
+app.use(express.json());
+
+router.use("/test", (req, res) => {
+  res.send("hello");
+});
+
+app.use("/api", router);
+
+app.listen(PORT, () => console.log(`Server is running at port -> ${PORT}`));
