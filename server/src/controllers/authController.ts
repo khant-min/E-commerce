876e101c08fb1fbe8397acf8d8a92c800c001385
Khant Min Thu <kmin01405@gmail.com>
2023-09-16 00:31:15 +0630

    feat: add register and login api

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,11 +1,49 @@
 import { Request, Response } from "express";
+import { UserProps } from "../types";
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-export const registerCustomer = (req: Request, res: Response) => {
-  const {} = req.body;
+export const registerCustomer = async (req: Request, res: Response) => {
+  const { name, email, password, phoneNumber }: UserProps = req.body;
+  if (!name || !email || !password || !phoneNumber)
+    return res.status(400).json({ message: "Please filled required fields" });
+
+  const findExistingUser = await prisma.user.findFirst({
+    where: {
+      OR: [{ email }, { phoneNumber }],
+    },
+  });
+
+  if (findExistingUser)
+    return res.status(409).json({ message: "User already exists" });
+
+  const salt = 10;
+  const hashPassword = await bcrypt.hash(password, salt);
+  console.log("reach here", hashPassword);
+
+  const user = await prisma.user.create({
+    data: { name, email, password: hashPassword, phoneNumber },
+  });
+
+  res.status(201).json(user);
 };
 
-export const loginCustomer = (req: Request, res: Response) => {
-  res.send("login!");
+export const loginCustomer = async (req: Request, res: Response) => {
+  const { email, password }: Pick<UserProps, "email" | "password"> = req.body;
+  if (!email || !password)
+    return res.status(400).json({ message: "Please filled required fields" });
+
+  const foundUser = await prisma.user.findFirst({ where: { email } });
+
+  if (!foundUser)
+    return res.status(401).json({ message: "Credentials are invalid" });
+
+  const matchPassword = await bcrypt.compare(password, foundUser.password);
+  if (!matchPassword)
+    return res.status(401).json({ message: "Credentials are invalid" });
+
+  res.status(200).json("User login!");
 };
 
 export const logoutCustomer = (req: Request, res: Response) => {
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import authRoute from "./routes/authRoute";
 import root from "./routes/root";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 const app = express();
 const router = express.Router({ mergeParams: true });
@@ -15,4 +17,30 @@ router.use("/auth", authRoute);
 
 app.use("/api", router);
 
-app.listen(PORT, () => console.log(`Server is running at port -> ${PORT}`));
+app.use(async () => {
+  console.log("connection closed...");
+
+  await prisma.$disconnect();
+});
+
+const server = app.listen(PORT, () =>
+  console.log(`Server is running at port -> ${PORT}`)
+);
+
+process.on("SIGINT", async () => {
+  console.log("Received SIGINT. Closing server and disconnecting Prisma.");
+  await prisma.$disconnect();
+  server.close(() => {
+    console.log("Server closed.");
+    process.exit(0);
+  });
+});
+
+process.on("SIGTERM", async () => {
+  console.log("Received SIGTERM. Closing server and disconnecting Prisma.");
+  await prisma.$disconnect();
+  server.close(() => {
+    console.log("Server closed.");
+    process.exit(0);
+  });
+});
