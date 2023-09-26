ea552d00526a2fc3773984dcdb8743c5c3581c1e
khant-min <kmin01405@gmail.com>
2023-09-26 11:16:44 +0630

    feat: implement reset password mechanism

--- server/src/controllers/serviceController.ts
+++ server/src/controllers/serviceController.ts
@@ -6,9 +6,8 @@ import { VerifyErrors } from "jsonwebtoken";
 import { AdminProps, DecodedData } from "../types";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import nodemailer from "nodemailer";
-import Mailgen from "mailgen";
 import MailService from "../utils/mail";
+import { generateCustomerOTP, generateAdminOTP } from "../utils/generateOTP";
 
 /**
  * Get Refresh Token
@@ -75,19 +74,29 @@ export const createNewAdmin = asyncHandler(async (req, res, next) => {
 });
 
 /**
- * @description sends OTP code to customer's email
- * @route POST /api/services/forgotPassword
+ * @description sends OTP code to customer's email and stores in session
+ * @route POST /api/services/getOTPCode
+ * @public (All)
  */
-export const forgotPassword = asyncHandler(async (req, res, next) => {
-  const { name, email } = req.body;
-  if (!name || !email)
+export const getOTPCode = asyncHandler(async (req, res, next) => {
+  const { name, email, role } = req.body;
+  if (!name || !email || !role)
     return next(new ErrorResponse("Please fill all required fields", 400));
+
+  const otpCode = role === "ADMIN" ? generateAdminOTP() : generateCustomerOTP();
+
+  res.cookie("otpCode", otpCode, {
+    // httpOnly: true,
+    // secure: true,
+    // sameSite: "none",
+    // maxAge: 5 * 60 * 1000,
+  });
 
   const message = {
     from: process.env.EMAIL,
     to: email,
     subject: "OTP Code",
-    html: MailService.mailGenerator(name),
+    html: MailService.mailGenerator(name, otpCode),
   };
 
   try {
@@ -98,4 +107,52 @@ export const forgotPassword = asyncHandler(async (req, res, next) => {
   } catch (err) {
     return next(new ErrorResponse("Nodemailer error", 500));
   }
+});
+
+/**
+ * @route POST /api/services/verify-otp
+ * @access public (All)
+ */
+export const verifyOTPCode = asyncHandler(async (req, res, next) => {
+  const { otpCode } = req.body;
+  const cookies = req.cookies;
+  if (!otpCode || !cookies?.otpCode)
+    return next(new ErrorResponse("Unauthorized", 401));
+
+  cookies.otpCode === otpCode
+    ? res.sendStatus(204)
+    : next(new ErrorResponse("Wrong OTP Code", 401));
+});
+
+/**
+ * @description validate OTP code
+ * @route POST /api/services/resetPassword
+ * @access public (All)
+ */
+export const resetPassword = asyncHandler(async (req, res, next) => {
+  const { id, passwrod, role } = req.body;
+  if (!id || !passwrod)
+    return next(new ErrorResponse("Please fill required fields", 400));
+
+  const foundUser =
+    role === "ADMIN"
+      ? await prisma.admin.findUnique({ where: { id } })
+      : await prisma.customer.findUnique({ where: { id } });
+
+  if (!foundUser) return next(new ErrorResponse("User not found", 404));
+
+  const salt = 10;
+  const hashedPassword = await bcrypt.hash(passwrod, salt);
+
+  role === "ADMIN"
+    ? await prisma.admin.update({
+        where: { id },
+        data: { password: hashedPassword },
+      })
+    : await prisma.customer.update({
+        where: { id },
+        data: { password: hashedPassword },
+      });
+
+  res.sendStatus(204);
 });
--- server/src/utils/generateOTP.ts
+++ server/src/utils/generateOTP.ts
@@ -1,14 +1,14 @@
 import crypto from "crypto";
 
 export function generateCustomerOTP(): string {
-  const randomDigits = crypto.randomBytes(3).readUIntBE(0, 3);
-  return String(randomDigits).padStart(6, "0");
+  const randomDigits = crypto.randomBytes(4).readUIntBE(0, 4);
+  return String(randomDigits).slice(0, 6);
 }
 
 export function generateAdminOTP(): string {
   const randomLetter1 = String.fromCharCode(
     65 + Math.floor(Math.random() * 26)
   );
-  const randomDigits = crypto.randomBytes(3).readUIntBE(0, 3);
-  return `${randomLetter1}-${String(randomDigits).padStart(8, "0")}`;
+  const randomDigits = crypto.randomBytes(4).readUIntBE(0, 4);
+  return `${randomLetter1}-${String(randomDigits).slice(0, 8)}`;
 }
