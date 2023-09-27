0b8f175ef056df228085ecfcafc970f4c4e7b034
khant-min <kmin01405@gmail.com>
2023-09-27 22:51:01 +0630

    feat: setup for admin portal

---
+++ AdminPortal/src/App.tsx
@@ -0,0 +1,9 @@
+import Button from "@mui/material/Button";
+
+export default function App() {
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
+}
---
+++ AdminPortal/src/redux/store/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
+import type { TypedUseSelectorHook } from "react-redux";
+
+export const store = configureStore({
+  reducer: {},
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
