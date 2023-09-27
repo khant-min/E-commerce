b4ca9921c5eb742ea435532f3c3a58de9ed9d58a
khant-min <kmin01405@gmail.com>
2023-09-27 18:55:09 +0630

    update: add _secure_ in admin routes

--- server/src/routes/serviceRoute.ts
+++ server/src/routes/serviceRoute.ts
@@ -6,11 +6,15 @@ const router = Router();
 
 // public
 router.post("/refresh", serviceController.refreshToken);
-router.post("/getOTPCode", verifyToken, serviceController.getOTPCode);
-router.post("/resetPassword", verifyToken, serviceController.resetPassword);
+// router.post("/get_otp", verifyToken, serviceController.getOTPCode);
+// router.post("/verify_otp", verifyToken, serviceController.verifyOTPCode);
+// router.post("/reset_password", verifyToken, serviceController.resetPassword);
+router.post("/get_otp", serviceController.getOTPCode);
+router.post("/verify_otp", serviceController.verifyOTPCode);
+router.post("/reset_password", serviceController.resetPassword);
 
 // private
-router.use("/secure", verifyAdmin);
-router.post("/secure/newAdmin", serviceController.createNewAdmin);
+router.use("/_secure_", verifyAdmin);
+router.post("/_secure_/new_admin", serviceController.createNewAdmin);
 
 export default router;
