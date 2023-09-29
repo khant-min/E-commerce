3d9556fb1ca1b68963c76f73208bacd6d3f28da4
khant-min <kmin01405@gmail.com>
2023-09-29 22:48:56 +0630

    update: lay structure

--- AdminPortal/src/App.tsx
+++ AdminPortal/src/App.tsx
@@ -1,9 +1,49 @@
-import Button from "@mui/material/Button";
+import { Routes, Route } from "react-router-dom";
+import "./index.css";
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="header">WELCOME TO QUICKPAY</h1>
+      <h3>Bank of the free</h3>
+      <p>Lorem ipsum dolor sit amet...</p>
+    </>
+  );
+};
+
+const Dashboard = () => {
+  return (
+    <>
+      <h1 className="header">DASHBOARD PAGE</h1>
+      <h3>Welcome User</h3>
+      <p>Lorem ipsum dolor sit amet...</p>
+    </>
+  );
+};
+
+const Transactions = () => {
+  return (
+    <>
+      <h1 className="header">KEEP TRACK OF YOUR SPENDINGS</h1>
+      <h3>Seamless Transactions</h3>
+      <p>Lorem ipsum dolor sit amet...</p>
+    </>
+  );
+};
 
 export default function App() {
   return (
-    <div>
-      <Button variant="contained">Hello World</Button>
+    <div style={{ display: "flex", height: "100vh" }}>
+      <section>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="transactions" element={<Transactions />} />
+        </Routes>
+      </section>
     </div>
   );
 }
+// #23B294
+// #5FBD72
+// #92C842
---
+++ AdminPortal/src/components/sidebar/Sider.tsx
@@ -0,0 +1,91 @@
+import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import GridViewRoundedIcon from "@mui/icons-material/GridViewRounded";
+import ReceiptRoundedIcon from "@mui/icons-material/ReceiptRounded";
+import BarChartRoundedIcon from "@mui/icons-material/BarChartRounded";
+import TimelineRoundedIcon from "@mui/icons-material/TimelineRounded";
+import BubbleChartRoundedIcon from "@mui/icons-material/BubbleChartRounded";
+import WalletRoundedIcon from "@mui/icons-material/WalletRounded";
+import AccountBalanceRoundedIcon from "@mui/icons-material/AccountBalanceRounded";
+import SavingsRoundedIcon from "@mui/icons-material/SavingsRounded";
+import MonetizationOnRoundedIcon from "@mui/icons-material/MonetizationOnRounded";
+import SettingsApplicationsRoundedIcon from "@mui/icons-material/SettingsApplicationsRounded";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import ShieldRoundedIcon from "@mui/icons-material/ShieldRounded";
+import NotificationsRoundedIcon from "@mui/icons-material/NotificationsRounded";
+import LogoutRoundedIcon from "@mui/icons-material/LogoutRounded";
+import { Link } from "react-router-dom";
+
+export default function Sider() {
+  return (
+    <div style={{ display: "flex", height: "100vh" }}>
+      <Sidebar
+        className="app"
+        style={{
+          padding: "0rem",
+          margin: "0rem",
+        }}
+        backgroundColor="inherith"
+      >
+        <Menu
+        // menuItemStyles={{
+        //   button: ({ level, active }) => {
+        //     if (level === 0 || level === 1) {
+        //       return {
+        //         backgroundColor: active ? "white" : undefined,
+        //         color: active ? "white" : undefined,
+        //         "&:hover": {
+        //           backgroundColor: "#F4FAFC",
+        //           color: "white",
+        //         },
+        //       };
+        //     }
+        //   },
+        // }}
+        >
+          {/* <MenuItem
+            component={<Link to="/" className="link" />}
+            className="menu1"
+            suffix={<MenuRoundedIcon />}
+          >
+            <h2>E-STORE</h2>
+          </MenuItem> */}
+          <SubMenu label="Menu" icon={<WalletRoundedIcon />}>
+            <MenuItem
+              component={<Link to="dashboard" className="link" />}
+              icon={<GridViewRoundedIcon />}
+            >
+              Dashboard
+            </MenuItem>
+            <MenuItem icon={<ReceiptRoundedIcon />}> Invoices </MenuItem>
+          </SubMenu>
+
+          <SubMenu label="Charts" icon={<BarChartRoundedIcon />}>
+            <MenuItem icon={<TimelineRoundedIcon />}> Timeline Chart </MenuItem>
+            <MenuItem icon={<BubbleChartRoundedIcon />}>Bubble Chart</MenuItem>
+          </SubMenu>
+          <SubMenu label="Wallets" icon={<WalletRoundedIcon />}>
+            <MenuItem icon={<AccountBalanceRoundedIcon />}>
+              Current Wallet
+            </MenuItem>
+            <MenuItem icon={<SavingsRoundedIcon />}>Savings Wallet</MenuItem>
+            <MenuItem
+              component={<Link to="transactions" className="link" />}
+              icon={<MonetizationOnRoundedIcon />}
+            >
+              Transactions
+            </MenuItem>
+          </SubMenu>
+          <SubMenu label="Settings" icon={<SettingsApplicationsRoundedIcon />}>
+            <MenuItem icon={<AccountCircleRoundedIcon />}> Account </MenuItem>
+            <MenuItem icon={<ShieldRoundedIcon />}> Privacy </MenuItem>
+            <MenuItem icon={<NotificationsRoundedIcon />}>
+              Notifications
+            </MenuItem>
+            <MenuItem icon={<LogoutRoundedIcon />}> Logout </MenuItem>
+          </SubMenu>
+        </Menu>
+      </Sidebar>
+    </div>
+  );
+}
