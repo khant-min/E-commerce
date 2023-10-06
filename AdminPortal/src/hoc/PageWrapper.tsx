668df27fa365f60f66117a0cb98182b667c03c7b
swamhtetaung <swamsuesha76@gmail.com>
2023-10-06 22:49:07 +0630

    add: login ui

--- AdminPortal/src/hoc/PageWrapper.tsx
+++ AdminPortal/src/hoc/PageWrapper.tsx
@@ -1,8 +1,32 @@
+import { useEffect } from "react";
 import Header from "../components/header/Header";
 import Sider from "../components/sidebar/Sider";
 import { ChildrenProps } from "../types";
+import AuthService from "../services/AuthService";
+import { Navigate } from "react-router-dom";
+import { useAppSelector } from "../redux/store/store";
 
 export default function PageWrapper({ children }: ChildrenProps) {
+  const accessToken = useAppSelector((state) => state.auth.accessToken);
+
+  const handleRefresh = async () => {
+    try {
+      await AuthService.refresh();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    const firstLogin = localStorage.getItem("firstLogin");
+    if (firstLogin === "Y") {
+      console.log("res");
+      handleRefresh();
+    }
+  }, []);
+
+  if (!accessToken) {
+    return <Navigate to={"/login"} />;
+  }
   return (
     <div>
       <Header />
