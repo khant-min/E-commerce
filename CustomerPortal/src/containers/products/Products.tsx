1ec6c21645830570d412315af029f8dbf654c930
JasontheOmnivorous <marsjason485@gmail.com>
2024-05-28 14:04:37 +0630

    feat(UI): product page with product cards

--- CustomerPortal/src/containers/products/Products.tsx
+++ CustomerPortal/src/containers/products/Products.tsx
@@ -1,3 +1,63 @@
+import { Box, Button, Input } from "@chakra-ui/react";
+import ProductCard from "../../components/products/ProductCard";
+
 export default function Products() {
-  return <div>Products</div>;
+  return (
+    <Box className="my-14 flex-col justify-center items-center w-full">
+      <Box className="flex justify-around items-center m-5">
+        <Input placeholder="Search products..." width={450} />
+        <Input
+          placeholder="Filter by price/ popularity (select list)..."
+          width={450}
+        />
+        <Button className="w-[35%]" colorScheme="purple">
+          Search
+        </Button>
+      </Box>
+      <Box className="bg-gray-200 flex justify-start items-center p-6 h-screen gap-5 overflow-x-scroll scroll-m-0 scrollbar-thin scrollbar-thumb-purple-600 scrollbar-track-slate-100">
+        <ProductCard
+          image="Image here"
+          name="Name here"
+          description="Description here"
+          price={99.99}
+        />
+        <ProductCard
+          image="Image here"
+          name="Name here"
+          description="Description here"
+          price={99.99}
+        />
+        <ProductCard
+          image="Image here"
+          name="Name here"
+          description="Description here"
+          price={99.99}
+        />
+        <ProductCard
+          image="Image here"
+          name="Name here"
+          description="Description here"
+          price={99.99}
+        />
+        <ProductCard
+          image="Image here"
+          name="Name here"
+          description="Description here"
+          price={99.99}
+        />
+        <ProductCard
+          image="Image here"
+          name="Name here"
+          description="Description here"
+          price={99.99}
+        />
+        <ProductCard
+          image="Image here"
+          name="Name here"
+          description="Description here"
+          price={99.99}
+        />
+      </Box>
+    </Box>
+  );
 }
--- CustomerPortal/src/hoc/AppContainer.tsx
+++ CustomerPortal/src/hoc/AppContainer.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
 
 export default function AppContainer() {
   return (
