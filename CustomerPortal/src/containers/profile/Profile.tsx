1e72ed900b77860e3061308d618c0ce23d197617
Khant Min Thu <kmin01405@gmail.com>
2024-06-23 21:26:02 +0630

    fix: update profile page

--- CustomerPortal/src/containers/profile/Profile.tsx
+++ CustomerPortal/src/containers/profile/Profile.tsx
@@ -1,25 +1,67 @@
-import { Avatar, Box, Button, Heading, Text } from "@chakra-ui/react";
+import React from "react";
+import {
+  Box,
+  Avatar,
+  Text,
+  Stack,
+  Button,
+  Badge,
+  HStack,
+  VStack,
+} from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import { MdOutlineEmail, MdOutlinePhone } from "react-icons/md";
 
+const MotionAvatar = motion(Avatar);
+
 export default function Profile() {
+  const user = JSON.parse(localStorage.getItem("user")!);
+  const getInitials = user.name
+    .split(" ")
+    .map((n: string) => n.charAt(0).toUpperCase())
+    .join("");
+
   return (
-    <Box className="min-h-[500px] flex flex-col items-center justify-center gap-10">
-      <Box className="flex gap-10">
-        <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
-        <Heading>John Doe</Heading>
-      </Box>
-      <Box className="flex flex-col gap-2">
-        <Text className="flex items-center gap-4">
-          <MdOutlineEmail /> johndoe20@gmail.com
+    <Box className="mt-20">
+      <Stack align="center" spacing={4}>
+        <MotionAvatar
+          size="2xl"
+          src={user.profile}
+          whileHover={{ rotate: 360 }}
+          transition={{ duration: 1 }}
+        />
+        <Text color="blue.600" fontSize="2xl" fontWeight="bold">
+          {user.name}
         </Text>
-        <Text className="flex items-center gap-4">
-          <MdOutlinePhone /> 09-86879485
-        </Text>
-      </Box>
-      <Box>
-        <Button>Change Password</Button>
-        <Button>Edit Profile</Button>
-      </Box>
+
+        <Box className="flex flex-col gap-2">
+          <Text className="flex items-center gap-4" fontSize="md" mt={2}>
+            <MdOutlineEmail fontSize={20} />
+            <p>{user.email}</p>
+          </Text>
+          <Text className="flex items-center gap-4" fontSize="md" mt={2}>
+            <MdOutlinePhone fontSize={20} />
+            <p>{user.phone}</p>
+          </Text>
+        </Box>
+
+        <HStack>
+          <Button
+            className="hover:scale-105"
+            colorScheme="blue"
+            variant="outline"
+          >
+            Change Password
+          </Button>
+          <Button
+            className="hover:scale-105"
+            colorScheme="blue"
+            variant="solid"
+          >
+            Edit Profile
+          </Button>
+        </HStack>
+      </Stack>
     </Box>
   );
 }
